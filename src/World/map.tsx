d8142e785b335fd7817ee535db0fde861e2a70fb
Kaos0w0 <brayan.andres.sanchez@correounivalle.edu.co>
2023-11-27 02:01:11 -0500

    El juego funciona. Faltan varios detalles de diseño, por ejemplo, mostrar en pantalla el ganador, ahora solo se muestra en consola

--- src/World/map.tsx
+++ src/World/map.tsx
@@ -1,22 +1,150 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { coordinate, matrix, coordinates } from '../Interfaces/interfaces';
-import { minimax } from '../scripts/minimax';
+import { minimax, posiblesMovientos } from '../scripts/minimax';
 
 interface MapaProps {
   difficulty: 'Facil' | 'Intermedio' | 'Dificil' | undefined;
 }
 
+let puntos_verde = 0;
+let puntos_rojo = 0;
+const divsConListeners: { div: HTMLElement; eventHandler: any; }[] = [];
+
+// Función que retorna el índice de un vector en una matriz (array de arrays)
+// Ej. matriz = [[1,2],[3,4]] vector = [3,4] retorna 1
+function indiceSubArray(matriz: coordinates, vector: coordinate) {
+  return matriz.findIndex((e) => e[0] === vector[0] && e[1] === vector[1]);
+}
+
+// Función que retorna true si un vector está en una matriz (array de arrays)
+// Ej. matriz = [[1,2],[3,4]] vector = [3,4] retorna true
+function existeSubArray(matriz: coordinates, vector: coordinate) {
+  return matriz.some((e) => e[0] === vector[0] && e[1] === vector[1]);
+}
+
+// Funcion que retorna true si existe un vector en una matriz cuyo primer elemento es igual al primer elemento del vector parámetro
+// Ej. matriz = [[1,2],[3,4]] vector = [3,4] retorna true
+function existeSubArrayConPrimerElementoIgual(matriz: coordinates, vector: coordinate) {
+  return matriz.some((e) => e[0] === vector[0]);
+}
+
+function juego_terminado(p_monedas_normales: coordinates, p_monedas_especiales: coordinates) {
+  return (p_monedas_especiales.length === 0 && p_monedas_normales.length === 0);
+}
+
+function animar_movimiento(posicion_inicial: coordinate, posicion_final: coordinate, matriz: matrix, p_jugadores: coordinates, tipo_moneda: string) {
+  const posicion_inicial_x = posicion_inicial[0];
+  const posicion_inicial_y = posicion_inicial[1];
+  const posicion_final_x = posicion_final[0];
+  const posicion_final_y = posicion_final[1];
+  const posicion_inicial_div = document.getElementById(`cell ${posicion_inicial_x}-${posicion_inicial_y}`);
+  const posicion_final_div = document.getElementById(`cell ${posicion_final_x}-${posicion_final_y}`);
+  const posicion_inicial_div_child = posicion_inicial_div?.firstChild;
+  const posicion_final_div_child = posicion_final_div?.firstChild;
+  if (posicion_inicial_div_child && posicion_final_div_child) {
+      posicion_inicial_div.removeChild(posicion_inicial_div_child);
+      posicion_final_div.removeChild(posicion_final_div_child);  
+      const image = document.createElement('img');
+      image.setAttribute('src', '../src/assets/elementos_juego/monedas/invisible.png');
+      image.setAttribute('width', '60%');
+      posicion_inicial_div.appendChild(image);
+      posicion_final_div.appendChild(posicion_inicial_div_child);
+  } 
+  //console.log(`fue de ${posicion_inicial_x}-${posicion_inicial_y} a ${posicion_final_x}-${posicion_final_y}`);
+  //console.log(matriz)
+}
+
+function actualizar_matriz(matriz: matrix, p_jugadores: coordinates, p_monedas_normales: coordinates, p_monedas_especiales: coordinates, mejor_jugada: coordinate, jugador: number) {
+  const indiceMoneda = indiceSubArray(p_monedas_normales, mejor_jugada);
+  const indiceMonedaEspecial = indiceSubArray(p_monedas_especiales, mejor_jugada);
+  const p_jugador = jugador === 3 ? 1 : 0;
+  animar_movimiento(p_jugadores[p_jugador], mejor_jugada, matriz, p_jugadores, 'normal');
+  matriz[mejor_jugada[0]][mejor_jugada[1]] = jugador;
+  if (indiceMoneda !== -1) {
+    jugador === 3 ? puntos_verde++ : puntos_rojo++;
+    p_monedas_normales.splice(indiceMoneda, 1);
+    matriz[mejor_jugada[0]][mejor_jugada[1]] = 5;
+  } else if (indiceMonedaEspecial !== -1) {
+    jugador === 3 ? puntos_verde += 3 : puntos_rojo += 3;
+    p_monedas_especiales.splice(indiceMonedaEspecial, 1);
+    matriz[mejor_jugada[0]][mejor_jugada[1]] = 5; 
+  }
+  if(matriz[p_jugadores[p_jugador][0]][p_jugadores[p_jugador][1]] !== 5){
+    matriz[p_jugadores[p_jugador][0]][p_jugadores[p_jugador][1]] = 0;
+  }  
+  p_jugadores[p_jugador] = mejor_jugada;  
+  const h3_puntaje_jugador = document.getElementById(jugador === 3 ? 'verde' : 'rojo');
+  if(h3_puntaje_jugador){
+    h3_puntaje_jugador.innerHTML = jugador === 3 ? `verde: ${puntos_verde}` : `rojo: ${puntos_rojo}`;
+  }
+}
+
+function turnoIA(matriz: matrix, p_monedas_normales: coordinates, p_monedas_especiales: coordinates, p_jugadores: coordinates, dificultad: 'Facil' | 'Intermedio' | 'Dificil' | undefined) {
+  if (juego_terminado(p_monedas_normales, p_monedas_especiales)) {
+    if(puntos_verde > puntos_rojo){
+      console.log('Gano VERDE');
+    } else if(puntos_verde < puntos_rojo){
+      console.log('Gano ROJO');
+    }
+    return;
+  } else {
+    const mejor_jugada = minimax(matriz, p_monedas_normales, p_monedas_especiales, p_jugadores, dificultad);
+    if(mejor_jugada === null){ return; }
+    actualizar_matriz(matriz, p_jugadores, p_monedas_normales, p_monedas_especiales, mejor_jugada, 3);
+    turnoHumano(matriz, p_monedas_normales, p_monedas_especiales, p_jugadores, dificultad);
+  } 
+}
+
+function turnoHumano(matriz: matrix, p_monedas_normales: coordinates, p_monedas_especiales: coordinates,  p_jugadores: coordinates, dificultad: 'Facil' | 'Intermedio' | 'Dificil' | undefined){
+  if (juego_terminado(p_monedas_normales, p_monedas_especiales)) {
+    if(puntos_verde > puntos_rojo){
+      console.log('Gano VERDE');
+    } else if(puntos_verde < puntos_rojo){
+      console.log('Gano ROJO');
+    }
+    return;
+  } else {
+    const movimientos = posiblesMovientos(p_jugadores[0], matriz);
+    console.log(movimientos);
+    for (let i = 0; i < movimientos.length; i++) {
+      agregarClickListenerADivs(`cell ${movimientos[i][0]}-${movimientos[i][1]}`, matriz, p_monedas_normales, p_monedas_especiales, p_jugadores, dificultad);
+    }
+  }
+}
+
+function agregarClickListenerADivs(divId: string, matriz: matrix, p_monedas_normales: coordinates, p_monedas_especiales: coordinates, p_jugadores: coordinates, dificultad: 'Facil' | 'Intermedio' | 'Dificil' | undefined) {
+  const div = document.getElementById(divId);
+  if (div) {
+    div.className = 'clickable';
+    function handleClick() {
+      console.log('click')
+      div ? div.removeEventListener('click', handleClick) : null;
+      div ? div.className = '' : null;
+      eliminarEventListenersDeDivs();
+      const jugada = divId.split(' ')[1].split('-').map(e => parseInt(e));
+      actualizar_matriz(matriz, p_jugadores, p_monedas_normales, p_monedas_especiales, (jugada as coordinate), 4);      
+      setTimeout(() => {
+        turnoIA(matriz, p_monedas_normales, p_monedas_especiales, p_jugadores, dificultad);
+      }, 2000);
+    }
+    div.addEventListener('click', handleClick);
+    divsConListeners.push({ div, eventHandler: handleClick });
+  }
+}
+
+function eliminarEventListenersDeDivs() {
+  divsConListeners.forEach(({ div, eventHandler }) => {
+    div.removeEventListener('click', eventHandler);
+    div.className = '';
+  });
+
+  // Limpiar la lista de divs con event listeners
+  divsConListeners.length = 0;
+}
+
 const Map: React.FC<MapaProps> = ({ difficulty }) => {
   let matrizYaFueCreada = false;
-
-  function buscarEnLista(lista: coordinates, elemento: coordinate) {
-    return lista.some((e) => e[0] === elemento[0] && e[1] === elemento[1]);
-  }
-
-  function buscarEnFila(lista: coordinates, elemento: coordinate) {
-    return lista.some((e) => e[0] === elemento[0]);
-  }
-
+  let matrizYaFueDibujada = false;
   let matriz_juego: matrix = Array(8).fill(0).map(() => Array(8).fill(0));
   let posicionMonedasNormales: coordinates = [[0, 0], [0, 7], [7, 0], [7, 7], [1, 0], [0, 1], [1, 7], [7, 1], [6, 0], [0, 6], [6, 7], [7, 6]];
   let posicionMonedasEspeciales: coordinates = [[3, 3], [3, 4], [4, 3], [4, 4]];
@@ -25,9 +153,9 @@ const Map: React.FC<MapaProps> = ({ difficulty }) => {
   function generarMatriz() {
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 8; j++) {
-        if (buscarEnLista(posicionMonedasNormales, [i, j])) {
+        if (existeSubArray(posicionMonedasNormales, [i, j])) {
           matriz_juego[i][j] = 1;
-        } else if (buscarEnLista(posicionMonedasEspeciales, [i, j])) {
+        } else if (existeSubArray(posicionMonedasEspeciales, [i, j])) {
           matriz_juego[i][j] = 2;
         } else {
           posicionesDisponibles.push([i, j]);
@@ -36,7 +164,7 @@ const Map: React.FC<MapaProps> = ({ difficulty }) => {
     }
     let posicionesAleatorias = 0;
     while (posicionesAleatorias < 2) {
-      //Primera posicion Jugador Rojo y segunda posicion Jugador Verde IA
+      // posicionJugadores = [[posicion yoshi rojo], [posicion yoshi verde]]]
       const randomIndex = Math.floor(Math.random() * posicionesDisponibles.length);
       const randomPosition = posicionesDisponibles[randomIndex];
       posicionesAleatorias == 1 ? matriz_juego[randomPosition[0]][randomPosition[1]] = 3 : matriz_juego[randomPosition[0]][randomPosition[1]] = 4;
@@ -45,8 +173,6 @@ const Map: React.FC<MapaProps> = ({ difficulty }) => {
       posicionesDisponibles.splice(randomIndex, 1);
     }
     matrizYaFueCreada = true;
-    //------------------------Prueba Minimax------------------------
-    minimax(matriz_juego, posicionMonedasNormales, posicionMonedasEspeciales, posicionesDisponibles, posicionJugadores, difficulty)
   }
 
 
@@ -63,24 +189,30 @@ const Map: React.FC<MapaProps> = ({ difficulty }) => {
         container.appendChild(row);
         for (let j = 0; j < 8; j++) {
           const div = document.createElement('div');
-          // div.style.border = '1px dashed #9e9c9c';
+          div.style.border = '1px dashed #9e9c9c';
           div.style.display = 'table-cell';
           div.style.textAlign = 'center';
           div.style.position = 'relative';
           div.style.width = '12.5%';
-          matriz_juego[i][j] === 0 ? div.style.paddingTop = '10.5%' : div.style.paddingTop = '0%';
-          if (matriz_juego[i][j] === 1) {
+          if (matriz_juego[i][j] === 0) {
+            const image = document.createElement('img');
+            image.setAttribute('src', '../src/assets/elementos_juego/monedas/invisible.png');
+            image.setAttribute('width', '55%');
+            div.appendChild(image);
+          } else if (matriz_juego[i][j] === 1) {
             const image = document.createElement('img');
             image.setAttribute('src', '../src/assets/elementos_juego/monedas/normal.gif');
             image.setAttribute('width', '40%');
-            buscarEnFila(posicionJugadores, [i, j]) ? div.style.transform = 'translateY(-10%)' : div.style.transform = 'translateY(10%)'
+            image.setAttribute('top', '50%');
+            existeSubArrayConPrimerElementoIgual(posicionJugadores, [i, j]) ? image.style.transform = 'translateY(-40%)' : image.style.transform = 'translateY(-20%)'
             div.appendChild(image);
           } else if (matriz_juego[i][j] === 2) {
             const image = document.createElement('img');
             image.setAttribute('src', '../src/assets/elementos_juego/monedas/especial.gif');
             image.setAttribute('width', '50%');
-            image.setAttribute('height', 'auto');
+            image.setAttribute('top', '50%');
             image.style.alignItems = 'center';
+            existeSubArrayConPrimerElementoIgual(posicionJugadores, [i, j]) ? image.style.transform = 'translateY(-10%)' : image.style.transform = 'translateY(5%)'
             div.appendChild(image);
           } else if (matriz_juego[i][j] === 3) {
             const image = document.createElement('img');
@@ -107,9 +239,14 @@ const Map: React.FC<MapaProps> = ({ difficulty }) => {
           row.appendChild(div);
         }
       }
+      if(!matrizYaFueDibujada){
+        matrizYaFueDibujada = true;
+        setTimeout(() => {
+          turnoIA(matriz_juego, posicionMonedasNormales, posicionMonedasEspeciales, posicionJugadores, difficulty);
+        }, 2000);
+      }
     }
   }, []);
-
 
   return (
     <>
@@ -127,8 +264,8 @@ const Map: React.FC<MapaProps> = ({ difficulty }) => {
           <br></br>
           <h3 id='turno'>turno de: verde</h3>
           <br></br>
-          <h3 id='rojo'>rojo: 0</h3>
-          <h3 id='verde'>verde: 0</h3>
+          <h3 id='rojo'>rojo: {puntos_rojo}</h3>
+          <h3 id='verde'>verde: {puntos_verde}</h3>
           <button id='exit' onClick={() => window.location.reload()}>salir</button>   
         </div>
       </div>
--- src/scripts/minimax.ts
+++ src/scripts/minimax.ts
@@ -2,7 +2,7 @@ import { Nodo } from "./classes";
 import * as Collections from 'typescript-collections';
 import { coordinate, coordinates, matrix } from "../Interfaces/interfaces";
 
-export function posibleMoviento(pos: coordinate): coordinates {
+export function posiblesMovientos(pos: coordinate, matriz: matrix): coordinates {
     let auxCoordinatesI: number[][] = [[2, -2], [1, -1]];
 
     //Posibles movimientos de pos
@@ -14,7 +14,9 @@ export function posibleMoviento(pos: coordinate): coordinates {
     let mov: coordinates = [];
     mov.push(...mov1, ...mov2, ...mov3, ...mov4)
     mov = mov.filter(pos => {
-        return pos[0] >= 0 && pos[0] <= 7 && pos[1] >= 0 && pos[1] <= 7
+        if (pos[0] >= 0 && pos[0] <= 7 && pos[1] >= 0 && pos[1] <= 7){
+            return (matriz[pos[0]][pos[1]] == 1 || matriz[pos[0]][pos[1]] == 0 || matriz[pos[0]][pos[1]] == 2)
+        }
     })
 
     return mov
@@ -40,8 +42,8 @@ export function heuristica(n_puntosIA: number, n_puntosJugador: number): number 
     return n_puntosIA - n_puntosJugador;
 }
 
-export function terminar(n_puntosIA: number, n_puntosJugador: number): boolean {
-    return ((n_puntosIA + n_puntosJugador) == 24)
+function juego_terminado(p_monedas_normales: coordinates, p_monedas_especiales: coordinates) {
+    return (p_monedas_especiales.length === 0 && p_monedas_normales.length === 0);
 }
 
 export function ganador(n_puntosIA: number, n_puntosJugador: number): number {
@@ -54,28 +56,37 @@ export function obtenerDificultad(dificultad:string): number {
             return 2;
         case "Intermedio":
             return 4;
-        case "hard":
+        case "Dificil":
             return 6;
     }
     return 0;
 }
 
-export function minimax(matrix: matrix, p_monedas: coordinates, p_monedas_especiales: coordinates, p_disponibles: coordinates, p_jugadores: coordinates, dificultad_juego: string = "Facil") {
-    matrix = matrix.slice();
-    p_disponibles = p_disponibles.slice();
+export function minimax(matrix: matrix, p_monedas: coordinates, p_monedas_especiales: coordinates, p_jugadores: coordinates, dificultad_juego: string = "Facil") {
     function crearArbol() {
         let NodoRaiz = new Nodo(null, p_jugadores, 0, 0, p_monedas, p_monedas_especiales, "MAX", -Infinity, 0);
         let pila = new Collections.Stack<Nodo>();
         let cola = new Collections.Queue<Nodo>();
         let profundidad = 0;
         pila.push(NodoRaiz);
-        let dificultad = obtenerDificultad(dificultad_juego); //Cambiar esto para cambiar la dificultad/profundidad del arbol
+        let dificultad = obtenerDificultad(dificultad_juego);
         while (profundidad < dificultad) {
+            if(juego_terminado(p_monedas, p_monedas_especiales)) {
+                let utilidad;
+                if(ganador(NodoRaiz.p_IA, NodoRaiz.p_Jugador) == 3) {
+                    utilidad = 1000;
+                } else {
+                    utilidad = -1000;
+                }
+
+                NodoRaiz.setUtilidad(utilidad);
+                break;
+            }
             profundidad++
             while (!pila.isEmpty()) {
                 let nodoActual = pila.pop()
                 if (nodoActual?.tipo == "MAX") {
-                    let movimientos = posibleMoviento(nodoActual.getPosicion("MAX"))
+                    let movimientos = posiblesMovientos(nodoActual.getPosicion("MAX"), matrix)
                     for (let movimiento of movimientos) {
                         let puntuacion = utilidadMovimiento(movimiento, p_monedas, p_monedas_especiales) + (nodoActual.padre?.p_IA ?? 0)
                         if (profundidad == dificultad) {
@@ -86,7 +97,7 @@ export function minimax(matrix: matrix, p_monedas: coordinates, p_monedas_especi
                         }
                     }
                 } else if (nodoActual?.tipo == "MIN") {
-                    let movimientos = posibleMoviento(nodoActual?.getPosicion("MIN"))
+                    let movimientos = posiblesMovientos(nodoActual?.getPosicion("MIN"), matrix)
                     for (let movimiento of movimientos) {
                         let puntuacion = utilidadMovimiento(movimiento, p_monedas, p_monedas_especiales) + (nodoActual.padre?.p_Jugador ?? 0)
                         if (profundidad == dificultad) {
@@ -132,7 +143,6 @@ export function minimax(matrix: matrix, p_monedas: coordinates, p_monedas_especi
         return NodoRaiz.mejor_mov;
     }
     let best_mov = crearArbol();
-    console.log(best_mov);
     return best_mov;
 
 }
